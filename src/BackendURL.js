14653a84d43cc713e4f0b73ad72adf07d1e4c1b3
Tsalsabilla <tsalsabilla569@gmail.com>
2025-08-04 21:38:16 +0700

    deploy

--- src/BackendURL.js
+++ src/BackendURL.js
@@ -1,4 +1,4 @@
-const BackendURL = "https://lms-studee-server.vercel.app"
+const BackendURL = "https://server-belajarin-tesis.vercel.app/"
 // const BackendURL = "http://localhost:4500"
 
 export default BackendURL;
